43c2985020a2651c70f56177b2c8adeb65b2a0bb
Thuku-Sam <samthuku21@gmail.com>
2024-03-20 17:15:43 +0300

    My first commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+name = 'James'
+age = input("How old are you? ")
+age = '30'
+location = input("Where are you from? ")
+location = 'Mombasa'
+print("Hello, " + name + "! You are " + age + " years old and you are from " + location + ". Nice to meet you!")
